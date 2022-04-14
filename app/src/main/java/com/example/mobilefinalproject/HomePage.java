3ad083ebdfe308cf96649a25ccf3b4389f0496b8
Harman Mangat <87164856+mangatharman@users.noreply.github.com>
2022-04-14 14:07:25 -0600

    Setting up the intents between screens
    Moving from one page to another without the functionality

--- app/src/main/java/com/example/mobilefinalproject/HomePage.java
+++ app/src/main/java/com/example/mobilefinalproject/HomePage.java
@@ -2,7 +2,10 @@ package com.example.mobilefinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        //Setting onClick Event on the Calendar button
+        Button btnCalendar = (Button) findViewById(R.id.btnCalendar);
+        btnCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalendar();
+            }
+        });
+
+        //Setting onClick Event on the New Event button
+        Button btnNewEvent = (Button) findViewById(R.id.btnNewEvent);
+        btnNewEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newEvent();
+            }
+        });
     }
+
+    //Method to go to the ViewPlanner Class/Screen
+    public void openCalendar() {
+        Intent intent = new Intent(this, ViewPlanner.class);
+        startActivity(intent);
+    }
+
+    //Method to go to the New Event Class/Screen
+    public void newEvent() {
+        Intent intent = new Intent(this, NewEvent.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/mobilefinalproject/MainActivity.java
+++ app/src/main/java/com/example/mobilefinalproject/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.mobilefinalproject;
 //Test comment
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting onClick Event on the Login Button
+        Button btnLogin = (Button) findViewById(R.id.loginButton);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
     }
+
+    //Method to go to the HomePage Class/Screen
+    public void login() {
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+
 }
